9c57c0d669a4899e4244b742a7398acc52435570
ryuto <rryuto5210@gmail.com>
2019-06-23 07:14:46 +0900

    Add null:false

--- db/migrate/20190622221359_create_posts.rb
+++ db/migrate/20190622221359_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.references :user, foreign_key: true
       t.references :elem, foreign_key: true
-      t.text :content
+      t.text :content, null: false
 
       t.timestamps
     end
