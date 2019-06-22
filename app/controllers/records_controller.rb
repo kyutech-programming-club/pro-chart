65ceaf9f84741e50086af12b2efbe07cb6591d25
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-06-23 01:00:33 +0900

    Save RecordElem in checkbox

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,13 +4,19 @@ class RecordsController < ApplicationController
 
   def new
     @record = Record.new
-    @record.record_elems.build
   end
 
   def create
+    puts params[:group][:elem_ids].map{|s| s.to_i}
     @record = Record.new(record_params)
     @record.user = current_user
+    # @record.record_elems.elem = Elem.find(params[:record][:record_elems_attributes]["0"]["elem_id"].map{|s| s.to_i})
     if @record.save
+      elems = params[:group][:elem_ids].map{|s| s.to_i}
+      elems.each do |e|
+        re = RecordElem.new(record_id: @record.id, elem_id: e)
+        re.save
+      end
       redirect_to record_path(@record)
     else
       render 'new'
@@ -22,6 +28,10 @@ class RecordsController < ApplicationController
 
   private
   def record_params
-    params.require(:record).permit(:r_date, :comment, record_elems_attributes:[:elem_id])
+    params.require(:record).permit(:r_date, :comment)
+  end
+
+  def elem_params
+    params.require(:group).permit(elem_ids: [])
   end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,4 @@
 class Record < ApplicationRecord
   belongs_to :user
   has_many :record_elems
-  accepts_nested_attributes_for :record_elems
 end
