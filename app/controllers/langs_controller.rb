ded08db1616b335eff6a6c287ff11112adc2103d
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-06-22 17:34:02 +0900

    Add controller of langs and add show-view of langs

--- app/controllers/langs_controller.rb
+++ app/controllers/langs_controller.rb
@@ -1,10 +1,28 @@
 class LangsController < ApplicationController
   def index
+    @langs = Lang.all
   end
 
   def new
+    @lang = Lang.new
+  end
+
+  def create
+    @lang = Lang.new(lang_params)
+    if @lang.save
+      redirect_to lang_path(@lang)
+    else
+      render :new
+    end
   end
 
   def show
+    @lang = Lang.find(params[:id])
+  end
+
+  private
+
+  def lang_params
+    params.required(:lang).permit(:name)
   end
 end
