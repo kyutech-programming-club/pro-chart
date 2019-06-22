eb4d79f468130a7b66ea39a30f59dc9156dc65a0
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-06-22 18:01:39 +0900

    Add routes of elems and db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'elems/index'
-  get 'elems/new'
-  get 'elems/show'
   root 'home#top'
   get "login" => "sessions#new"
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
   resources 'users'
-  resources 'langs'
+  resources 'langs' do
+    resources 'elems'
+  end
 
 end
