8f81fbe835ec8f618514cd16c1b1365449667156
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-06-22 18:56:40 +0900

    Add elems flash messages

--- app/controllers/elems_controller.rb
+++ app/controllers/elems_controller.rb
@@ -8,7 +8,7 @@ class ElemsController < ApplicationController
     lang = Lang.find(params[:lang_id])
     @elem = lang.elems.build(elem_params)
     if @elem.save
-      redirect_to root_path
+      redirect_to root_path, success: "要素の登録に成功しました"
     else
       render :new
     end
