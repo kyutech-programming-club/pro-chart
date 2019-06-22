29713137dcabc3df7f63d2a8c9fc1246a204d809
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-06-22 16:46:50 +0900

    conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources 'users'
 end
