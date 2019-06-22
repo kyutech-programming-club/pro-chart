abde8b907b461a4fbe13f1fee45078ed4f5e175f
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-06-22 17:52:30 +0900

    rails g controller Elems index new show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'elems/index'
+  get 'elems/new'
+  get 'elems/show'
   root 'home#top'
   get "login" => "sessions#new"
   post "login" => "sessions#create"
