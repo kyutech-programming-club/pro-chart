64c938a45b304639d85330aa1ab98eb129139700
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-06-22 15:52:22 +0900

    Add actions of user-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+      end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.required(:user).permit(:name, :password, :password_confirmation)
   end
 end
