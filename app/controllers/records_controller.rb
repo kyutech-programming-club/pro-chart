6862d6c399ba3fb921cdf5a4e29213fbab2851d1
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-06-22 22:46:24 +0900

    Restore lang and elem

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,8 +3,25 @@ class RecordsController < ApplicationController
   end
 
   def new
+    @record = Record.new
+    @record.record_elems.build
+  end
+
+  def create
+    @record = Record.new(record_params)
+    @record.user = current_user
+    if @record.save
+      redirect_to record_path(@record)
+    else
+      render 'new'
+    end
   end
 
   def show
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:r_date, :comment, record_elems_attributes:[:elem_id])
   end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,5 @@
 class Record < ApplicationRecord
   belongs_to :user
   has_many :record_elems
+  accepts_nested_attributes_for :record_elems
 end
