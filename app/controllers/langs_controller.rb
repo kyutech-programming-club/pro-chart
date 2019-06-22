88756b590e20f9af10c2dd2f1811bf54f55f06c3
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-06-22 17:06:48 +0900

    rails g controller Langs index new show

---
+++ app/controllers/langs_controller.rb
@@ -0,0 +1,10 @@
+class LangsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'langs/index'
+  get 'langs/new'
+  get 'langs/show'
   root 'users#index'
   get "login" => "sessions#new"
   post "login" => "sessions#create"
