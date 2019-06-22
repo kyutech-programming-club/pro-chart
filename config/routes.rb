5a1e5d841765836fae55690291d55d01759da5e9
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-06-22 16:11:53 +0900

    Add login and logout functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+
   root 'users#index'
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
   resources 'users'
 end
