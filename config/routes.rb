d359f546602e7d4b161be76b2ebcaebf1d7b1abe
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-06-22 17:15:10 +0900

    Add routes of langs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'langs/index'
-  get 'langs/new'
-  get 'langs/show'
   root 'users#index'
   get "login" => "sessions#new"
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
   resources 'users'
+  resources 'langs'
+
 end
